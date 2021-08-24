e4cde42705215b385978024da2b4aa5ebb0fd696
Savi Z <savi_z@hotmail.com>
2021-08-24 18:36:45 -0300

    carttt

--- src/components/cartwidget/CartWidget.jsx
+++ src/components/cartwidget/CartWidget.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import {MdShoppingCart} from "react-icons/md";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
-import "../cartwidget/cartwidget.css";
+import "../cartwidget/CartWidget.css";
 
 const IconoCarrito = () => {
 
